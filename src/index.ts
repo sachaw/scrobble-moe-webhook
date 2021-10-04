af60c5e6c3675d59a1869fdbef203669bb6440fb
Sacha Weatherstone <sachaw100@hotmail.com>
2021-10-04 22:20:35 +1100

    Fixes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import asyncBusboy from 'async-busboy';
 import { config } from 'dotenv';
 import { gql, GraphQLClient } from 'graphql-request';
-import { urlencoded } from 'milliparsec';
+import multiparty from 'multiparty';
 
 import { App } from '@tinyhttp/app';
 import { logger } from '@tinyhttp/logger';
@@ -14,55 +13,54 @@ const app = new App();
 
 void app
   .use(logger())
-  .use(urlencoded())
   .post("/:secret", async (req, res) => {
     const { secret } = req.params;
-    const { fields } = await asyncBusboy(req);
 
-    console.log(fields);
+    const form = new multiparty.Form();
 
-    if (fields.payload) {
-      console.log(fields.payload);
+    form.parse(req, async (err, fields) => {
+      if (fields.payload) {
+        const payload: IplexWebhook = JSON.parse(fields.payload);
+        const match = payload.Metadata.guid.match(
+          /me\.sachaw\.agents\.anilist:\/\/(?<id>.*)\/[0-9]\//
+        )?.groups;
+        const providerMediaId = match?.id ?? "";
 
-      const payload: IplexWebhook = JSON.parse(fields.payload);
-      const match = payload.Metadata.guid.match(
-        /me\.sachaw\.agents\.anilist:\/\/(?<id>.*)\/[0-9]\//
-      )?.groups;
-      const providerMediaId = match?.id ?? "";
+        if (payload.event === "media.scrobble" && providerMediaId) {
+          console.log(
+            `Incomming scrobble - user: ${payload.Account.id} Provider ID: ${providerMediaId} Episode: ${payload.Metadata.index}`
+          );
 
-      if (payload.event === "media.scrobble" && providerMediaId) {
-        console.log(
-          `Incomming scrobble - user: ${payload.Account.id} Provider ID: ${providerMediaId} Episode: ${payload.Metadata.index}`
-        );
+          const graphQLClient = new GraphQLClient(process.env.GQL_URL ?? "");
 
-        const graphQLClient = new GraphQLClient(process.env.GQL_URL ?? "");
-
-        const mutation = gql`
-          mutation scrobble($scrobbleWebhookInput: WebhookInput!) {
-            scrobble(webhookInput: $scrobbleWebhookInput) {
-              success
-              reason
+          const mutation = gql`
+            mutation scrobble($scrobbleWebhookInput: WebhookInput!) {
+              scrobble(webhookInput: $scrobbleWebhookInput) {
+                success
+                reason
+              }
             }
+          `;
+          const variables = {
+            scrobbleWebhookInput: {
+              secret,
+              username: payload.Account.title,
+              serverUUID: payload.Server.uuid,
+              providerMediaId: parseInt(providerMediaId),
+              episode: payload.Metadata.index,
+            },
+          };
+
+          try {
+            console.log(await graphQLClient.request(mutation, variables));
+          } catch (e) {
+            console.log(e);
           }
-        `;
-        const variables = {
-          scrobbleWebhookInput: {
-            secret,
-            username: payload.Account.title,
-            serverUUID: payload.Server.uuid,
-            providerMediaId: parseInt(providerMediaId),
-            episode: payload.Metadata.index,
-          },
-        };
-        try {
-          console.log(await graphQLClient.request(mutation, variables));
-        } catch (e) {
-          console.log(e);
         }
       }
-    }
+    });
 
-    res.send(null);
+    res.sendStatus(200);
   })
   .get("/", (_, res) => {
     res.sendStatus(200);
