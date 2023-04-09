8d7267d6d0638499eab56dcdd47e5ef5fa7f6026
Sacha Weatherstone <sachaw100@hotmail.com>
2023-04-09 18:18:00 +1000

    Repo cleanup

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # scrobble.moe Webhook forwarder
 
-[![Open in Visual Studio Code](https://open.vscode.dev/badges/open-in-vscode.svg)](https://open.vscode.dev/scrobble-moe/webhook)
-
 ## Overview
 
 This service forwards [Plex](https://plex.tv) webhooks to the main [scrobble.moe](https://github.com/scrobble-moe/server) GraphQL server.
--- src/index.ts
+++ src/index.ts
@@ -9,26 +9,26 @@ config();
 const app = new App();
 
 void app
-  .post("/:secret", async (req, res) => {
-    const { secret } = req.params;
+	.post("/:secret", async (req, res) => {
+		const { secret } = req.params;
 
-    const form = new multiparty.Form();
+		const form = new multiparty.Form();
 
-    form.parse(req, async (err, fields) => {
-      if (fields.payload) {
-        const payload: IplexWebhook = JSON.parse(fields.payload);
-        if (!payload.Metadata) {
-          return;
-        }
-        const match = payload.Metadata.guid?.match(
-          /me\.sachaw\.agents\.anilist:\/\/(?<id>.*)\/[0-9]\//
-        );
-        const providerMediaId = match?.groups?.id ?? "";
+		form.parse(req, async (err, fields) => {
+			if (fields.payload) {
+				const payload: IplexWebhook = JSON.parse(fields.payload);
+				if (!payload.Metadata) {
+					return;
+				}
+				const match = payload.Metadata.guid?.match(
+					/me\.sachaw\.agents\.anilist:\/\/(?<id>.*)\/[0-9]\//,
+				);
+				const providerMediaId = match?.groups?.id ?? "";
 
-        if (payload.event === "media.scrobble" && providerMediaId) {
-          const graphQLClient = new GraphQLClient(process.env.GQL_URL ?? "");
+				if (payload.event === "media.scrobble" && providerMediaId) {
+					const graphQLClient = new GraphQLClient(process.env.GQL_URL ?? "");
 
-          const mutation = gql`
+					const mutation = gql`
             mutation scrobble($scrobbleWebhookInput: WebhookInput!) {
               scrobble(webhookInput: $scrobbleWebhookInput) {
                 success
@@ -36,35 +36,35 @@ void app
               }
             }
           `;
-          const variables = {
-            scrobbleWebhookInput: {
-              secret,
-              username: payload.Account.title,
-              serverUUID: payload.Server.uuid,
-              providerMediaId: parseInt(providerMediaId),
-              episode: payload.Metadata.index,
-            },
-          };
+					const variables = {
+						scrobbleWebhookInput: {
+							secret,
+							username: payload.Account.title,
+							serverUUID: payload.Server.uuid,
+							providerMediaId: parseInt(providerMediaId),
+							episode: payload.Metadata.index,
+						},
+					};
 
-          await graphQLClient.request(mutation, variables).catch((e) => {
-            console.error(e, {
-              event: payload.event,
-              providerMediaId,
-              episode: payload.Metadata?.index?.toString() ?? "",
-              user: payload.Account.id,
-              username: payload.Account.title,
-              owner: payload.owner,
-            });
-          });
-        }
-      }
-    });
+					await graphQLClient.request(mutation, variables).catch((e) => {
+						console.error(e, {
+							event: payload.event,
+							providerMediaId,
+							episode: payload.Metadata?.index?.toString() ?? "",
+							user: payload.Account.id,
+							username: payload.Account.title,
+							owner: payload.owner,
+						});
+					});
+				}
+			}
+		});
 
-    res.sendStatus(200);
-  })
-  .get("/", (_, res) => {
-    res.sendStatus(200);
-  })
-  .listen(parseInt(process.env.PORT ?? "5000"), () =>
-    console.log(`🚀 Server ready`)
-  );
+		res.sendStatus(200);
+	})
+	.get("/", (_, res) => {
+		res.sendStatus(200);
+	})
+	.listen(parseInt(process.env.PORT ?? "5000"), () =>
+		console.log("🚀 Server ready"),
+	);
